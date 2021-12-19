57b1f8c2b2b3b02bf168c399c5e0fa10f6ded23a
Duncan Champney <DChampney@wareto.com>
2021-12-19 10:23:11 -0500

    Initial commit

--- UnderlinedTextField/ViewController.swift
+++ UnderlinedTextField/ViewController.swift
@@ -13,7 +13,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
